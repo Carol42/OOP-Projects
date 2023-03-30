753954325c796b6c19a4e342b36043309cedaa58
Carol42 <caroline.heliose@gmail.com>
2023-03-30 14:39:50 -0300

    :rocket: Aula 02

--- Conta Corrente/ContaCorrente.java
+++ Conta Corrente/ContaCorrente.java
@@ -1,8 +1,38 @@
 public class ContaCorrente {
-    public String nome;
+    private String nome;
     private int numero;
     private short agencia;
     private double saldo;
+    private double limite;
+
+    public ContaCorrente(String nome, int numero, short agencia, double saldo, double limite){
+        this.nome = nome;
+        this.numero = numero;
+        this.agencia = agencia;
+        this.saldo = saldo;
+        this.limite = limite;
+    }
+    public ContaCorrente(String nome, int numero, short agencia, double saldo){
+        this.nome = nome;
+        this.numero = numero;
+        this.agencia = agencia;
+        this.saldo = saldo;
+    }
+    public ContaCorrente(String nome, int numero, short agencia){
+        this.nome = nome;
+        this.numero = numero;
+        this.agencia = agencia;
+    }
+    public String getNome(String n){
+        return nome;
+    }
+
+    public void setNome(String n){
+        if(n.length()>=3)
+            nome=n;
+        else
+            System.out.println("O nome não pode ter menos que 3 caracteres");
+    }
 
     public int getNumero(){
         return numero;
@@ -32,6 +62,16 @@ public class ContaCorrente {
         saldo=valor;
     }
 
+    public double getLimite(double valor){
+        return limite;
+    }
+
+    public void setLimite(double valor){
+        if(valor>=0)
+            limite=valor;
+        else System.out.println("O limite não pode ser menor que zero.");
+    }
+
     public void sacar(double valor){
         if(valor>0 && valor<=saldo){
             saldo=saldo-valor;
@@ -46,11 +86,23 @@ public class ContaCorrente {
         else System.out.println("O valor a depositar não pode ser negativo");
     }
 
+    public void atualizar(double saldo, double limite){
+        this.saldo = saldo;
+        this.limite = limite;
+    }
+
+    public void atualizar(String nome, int numero, short agencia){
+        this.nome = nome;
+        this.numero = numero;
+        this.agencia = agencia;
+    }
+
     public void imprimir(){
         System.out.println("Nome do titular: "+nome);
         System.out.println("Número da conta: "+numero);
         System.out.println("Agência "+agencia);
-        System.out.println("Saldo: "+saldo+"\n");
+        System.out.println("Saldo: "+saldo);
+        System.out.println("Limite: "+limite+"\n");
 
     }
 }
